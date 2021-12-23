288ed5e4236443a6417620de4c008449a2f474a7
aslam <aslam.b@inqutec.com>
2021-12-23 11:14:13 +0530

    build apk path changed in script

--- Assets/Editor/Builder/AndroidBuilder.cs
+++ Assets/Editor/Builder/AndroidBuilder.cs
@@ -5,7 +5,7 @@ class AndroidBuilder
     {
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
         buildPlayerOptions.scenes = new[] { "Assets/Scenes/Scene1.unity", "Assets/Scenes/SampleScene.unity" };
-        buildPlayerOptions.locationPathName = "/Builds/CICDTest.apk";
+        buildPlayerOptions.locationPathName = "/builds/CICDTest.apk";
         buildPlayerOptions.target = BuildTarget.Android;
         buildPlayerOptions.options = BuildOptions.None;
         BuildPipeline.BuildPlayer(buildPlayerOptions);
